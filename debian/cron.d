78677a6b3d281afee6c38ed11c7e9e736e25977d
Taku YASUI <tach@debian.org>
2007-09-02 15:02:45 +0000

    error fix
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/hlbackup/debian/hlbackup/trunk@15 7c90d983-10fa-0310-8751-8832a4c28615

--- debian/cron.d
+++ debian/cron.d
@@ -1,1 +1,1 @@
-0 3	* * *	root	test -x /usr/sbin/hlbackup && /usr/sbin/hlbackup
+0 2	* * *	root	(test -x /usr/sbin/hlbackup && /usr/sbin/hlbackup); (test -x /usr/sbin/hlbackup-clean && /usr/sbin/hlbackup-clean /backup)
