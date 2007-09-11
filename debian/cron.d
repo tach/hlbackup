f9844718095bf59d88ee50bbbd129e8640f2ea4c
Taku YASUI <tach@debian.org>
2007-09-11 05:06:00 +0000

    fix to run clean
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/hlbackup/debian/hlbackup/trunk@18 7c90d983-10fa-0310-8751-8832a4c28615

--- debian/cron.d
+++ debian/cron.d
@@ -1,1 +1,1 @@
-0 2	* * *	root	(test -x /usr/sbin/hlbackup && /usr/sbin/hlbackup); (test -x /usr/sbin/hlbackup-clean && /usr/sbin/hlbackup-clean /backup)
+0 2	* * *	root	(test -x /usr/sbin/hlbackup && /usr/sbin/hlbackup); (test -x /usr/sbin/hlbackup-clean && /usr/sbin/hlbackup-clean /backup/*)
